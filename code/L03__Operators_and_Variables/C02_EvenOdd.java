5979329b7ed961e49625b8c3373249c5997f139a
avionics18 <avimurmu18@gmail.com>
2025-06-30 21:00:07 +0530

    minor changes

--- code/L03__Operators_and_Variables/C02_EvenOdd.java
+++ code/L03__Operators_and_Variables/C02_EvenOdd.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 
-class C02_EvenOdd {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter a number = ");
-    int n = sc.nextInt();
+public class C02_EvenOdd {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number = ");
+		int n = sc.nextInt();
+		sc.close();
 
-    if (n % 2 == 0) {
-      System.out.println("Number is even.");
-    } else {
-      System.out.println("Number is Odd.");
-    }
-  }
+		if (n % 2 == 0) {
+			System.out.println("Number is even.");
+		} else {
+			System.out.println("Number is Odd.");
+		}
+	}
 }
--- code/L03__Operators_and_Variables/C03_SumN.java
+++ code/L03__Operators_and_Variables/C03_SumN.java
@@ -1,18 +1,20 @@
+
 // Find the sum of n natural numbers
 import java.util.Scanner;
 
-class C03_SumN {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter the value of n = ");
-    int n = sc.nextInt();
+public class C03_SumN {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the value of n = ");
+		int n = sc.nextInt();
+		sc.close();
 
-    int sum = 0;
-    while (n > 0) {
-      sum += n;
-      n--;
-    }
+		int sum = 0;
+		while (n > 0) {
+			sum += n;
+			n--;
+		}
 
-    System.out.println("Sum = " + sum);
-  }
+		System.out.println("Sum = " + sum);
+	}
 }
